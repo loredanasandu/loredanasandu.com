bc2081cd3819289f9adeb87b5057fbf7b2c6f969
Loredana Sandu <loredanasandu.build@gmail.com>
2022-11-10 12:11:01 +0100

    fix dark mode

--- components/Title.jsx
+++ components/Title.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Title = () => {
     return (
         <nav>
-            <Link href='/' style={{ textDecoration: 'none', color:'black' }} passHref>
+            <Link href='/' passHref>
                 <h1 className='title'>Loredana Sandu</h1>
             </Link>
         </nav>
